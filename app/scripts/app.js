6b5e441c887956cc8c8cab44b259639a3916d4f0
unknown <Rhayme00@gmail.com>
2014-08-27 23:33:25 -0600

    my first push

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name finalProjectApp
+ * @description
+ * # finalProjectApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('finalProjectApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name finalProjectApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the finalProjectApp
+ */
+var app = angular.module('ShowsTrack', []);
+    var 
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
